c580528a8fab07141eb221a3448da983841be5fc
Bongani Nyoni <b.nyoni1101@gmail.com>
2023-08-05 09:06:57 +0200

    Adding Projects to Solution

---
+++ CruiseManagement.Api/Entities/BookingAgent.cs
@@ -0,0 +1,9 @@
+﻿namespace CruiseManagement.Api.Entities
+{
+    public class BookingAgent
+    {
+        public int Id { get; set; }
+        public string AgentName { get; set; }
+        public int AgentTypeId { get; set; }
+    }
+}
---
+++ CruiseManagement.Api/Entities/Reservation.cs
@@ -0,0 +1,13 @@
+﻿namespace CruiseManagement.Api.Entities
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int AgentId { get; set; }
+        public int UserId { get; set; }
+        public string Notes { get; set; }
+    }
+}
